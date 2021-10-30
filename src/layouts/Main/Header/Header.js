b9a2c4df170ad6a0612b66d11e51b0b8b73ef2fd
EmptyG2018 <Empty.G.2018@Gmail.com>
2021-10-31 00:06:44 +0800

    导航操作按钮icon

--- src/layouts/Main/Header/Header.js
+++ src/layouts/Main/Header/Header.js
@@ -1,12 +1,17 @@
 import HeaderStyle from "./Header.module.less";
+import SvgIcon from "../../../components/SvgIcon";
 function Header() {
 
   return (
     <div className={HeaderStyle.header}>
       <div className={HeaderStyle.aside}>
         <div className={HeaderStyle.navigation}>
-          <div className={HeaderStyle.navigation_btn}></div>
-          <div className={HeaderStyle.navigation_btn}></div>
+          <div className={`${HeaderStyle.navigation_btn} ${HeaderStyle.back}`}>
+            <SvgIcon iconClass="arrow-right"></SvgIcon>
+          </div>
+          <div className={`${HeaderStyle.navigation_btn} ${HeaderStyle.next}`}>
+            <SvgIcon iconClass="arrow-right"></SvgIcon>
+          </div>
         </div>
         <div className={HeaderStyle.search}></div>   
       </div>
